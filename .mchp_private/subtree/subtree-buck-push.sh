4876809e97e3a511b08f980c5ecfcb3b2b440ff7
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2021-02-16 01:49:40 +0100

    Updated subtree management file PUSH

--- .mchp_private/subtree/subtree-buck-push.sh
+++ .mchp_private/subtree/subtree-buck-push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo $SSH_AUTH_SOCK
 cd ../../
-git subtree push --prefix="dpsk_buck_vmc.X/sources/power_control/devices" subrepo-buck feature/version-update
+git subtree push --prefix="dpsk_buck_vmc.X/sources/power_control/devices" subrepo-buck split:feature/version-update2
 echo Press Enter to exit
 read
 
