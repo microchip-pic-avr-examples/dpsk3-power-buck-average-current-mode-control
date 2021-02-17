0cd8f3a18bf2cda9ce3850b202ea92a5fc8c17a5
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2021-02-17 14:42:18 +0100

    Updated buck device driver subtree management file

--- .mchp_private/subtree/subtree-buck-pull.sh
+++ .mchp_private/subtree/subtree-buck-pull.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 echo $SSH_AUTH_SOCK
 cd ../../
-git subtree pull --prefix="dpsk_buck_vmc.X/sources/power_control/devices" subrepo-buck main --squash 
+#echo Splitting Subtree subrepo-buck into working branch subrepo-buck-update...
+#git subtree split --prefix="dpsk_buck_vmc.X/sources/power_control/devices" --annotate='(split)' --branch 'subrepo-buck-update'
+
+#echo Switch to branch subrepo-buck-update...
+#git checkout subrepo-buck-update
+#git gitk subrepo-buck-update
+#$ git push "https://bitbucket.microchip.com/scm/mcu16asmpsl/subrepo-buck-converter.git" subrepo-buck:version-update
+
+git subtree pull --prefix="dpsk_buck_vmc.X/sources/power_control/devices" subrepo-buck main --no-squash 
 echo Press Enter to exit
 read
