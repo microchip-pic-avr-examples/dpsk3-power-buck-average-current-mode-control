da892678806496069ff0087a38fc065190cbd6f6
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2021-02-22 12:33:53 +0100

    Updated subtree management script of buck device driver repo

--- .mchp_private/subtree/subtree-buck-push.sh
+++ .mchp_private/subtree/subtree-buck-push.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo $SSH_AUTH_SOCK
 cd ../../
-git subtree push --prefix="dpsk_buck_vmc.X/sources/power_control/devices" subrepo-buck split:feature/version-update2
+#git subtree push --prefix="dpsk_buck_vmc.X/sources/power_control/devices" subrepo-buck split:feature/version-update2
+git subtree push --prefix="dpsk_buck_vmc.X/sources/power_control/devices" subrepo-buck feature/version-update --squash
 echo Press Enter to exit
 read
 
