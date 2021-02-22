dc1d67c935888845d35fc1b65dc69aeb1ad36b19
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2021-02-22 16:26:33 +0100

    Updated subtree management scripts

--- .mchp_private/subtree/subtree-buck-push.sh
+++ .mchp_private/subtree/subtree-buck-push.sh
@@ -3,7 +3,7 @@ echo $SSH_AUTH_SOCK
 cd ../../
 echo Pushing changes to subtree 'subrepo-buck' branch 'version-update' from recently checked out working branch...
 git subtree push --prefix="dpsk_buck_vmc.X/sources/power_control/devices" subrepo-buck feature/version-update --squash
-echo Pushing changes to subtree complete
+echo Pushing changes to subtree directory complete
 echo
 echo Press Enter to exit
 read
